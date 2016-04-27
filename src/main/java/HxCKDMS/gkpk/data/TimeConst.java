3470da3640a22d652977967370f5f3a710f6b28b
DrZed <kyahne14@gmail.com>
2016-04-27 01:00:32 -0400

    Goodnight (added todo)

--- src/main/java/HxCKDMS/gkpk/data/TimeConst.java
+++ src/main/java/HxCKDMS/gkpk/data/TimeConst.java
@@ -1,8 +1,9 @@
-// Class shamelessy reused from minechem 
+// Class shamelessy reused from minechem
+//TODO Rename to lib, and change to HxC format and then remove above comment
 package HxCKDMS.gkpk.data;
 public class TimeConst {
-    public static final int TICKS_PER_SECOND 	= 20;
-    public static final int TICKS_PER_MINUTE 	= TICKS_PER_SECOND * 60;
+    public static final int TICKS_PER_SECOND  = 20;
+    public static final int TICKS_PER_MINUTE  = TICKS_PER_SECOND * 60;
     public static final int FERMENTER_TIME = TICKS_PER_SECOND * 10;
     public static final int EXTRACTOR_TIME = TICKS_PER_SECOND * 15;
 }
