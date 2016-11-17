3f73286f218a98ce342dee79af690214aa7f3c89
MFernflower <machacker9000@gmail.com>
2016-11-17 13:40:12 -0500

    Im an idiot....

--- src/main/java/HxCKDMS/gkpk/items/ItemPill.java
+++ src/main/java/HxCKDMS/gkpk/items/ItemPill.java
@@ -4,14 +4,12 @@ import HxCKDMS.gkpk.client.GordianCreativeTab;
 import net.minecraft.item.ItemFood;
 
 public class ItemPill extends ItemFood{
-    public ItemPill()
+    public ItemPill(int a, int b, boolean c)
     {
         super(0, 0, false);
-        setMaxDamage(0);
-        setMaxStackSize(32);
         this.setCreativeTab(GordianCreativeTab.gordianCreativeTab);
         this.setUnlocalizedName("gPill");
         this.setTextureName("GKPK:pillitem");
-        this.setMaxStackSize(32);
+        this.setMaxStackSize(64);
     }
 }
